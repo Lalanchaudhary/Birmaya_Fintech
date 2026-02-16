e3060da0f7c0fedb422d1490aae333a3abcc55cb
Lalan Chaudhary <chaudharylalan28@gmail.com>
2026-02-16 12:54:51 +0530

    zfxcv

--- components/contact/ContactInfo.js
+++ components/contact/ContactInfo.js
@@ -8,7 +8,7 @@ export default function ContactInfo() {
         {/* LEFT — Google Map */}
         <div className="w-full h-[400px] rounded-2xl overflow-hidden shadow-lg">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3503.585232977682!2d77.31323167528714!3d28.582215075692247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce4594b84a389%3A0x4440c550c363a18c!2s55%2C%20FM%20Rd%2C%20D%20Block%2C%20Sector%202%2C%20Noida%2C%20Uttar%20Pradesh%20201301!5e0!3m2!1sen!2sin!4v1771062443440!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3503.576770945629!2d77.31302527549893!3d28.58246907569207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDM0JzU2LjkiTiA3N8KwMTgnNTYuMiJF!5e0!3m2!1sen!2sin!4v1771226496993!5m2!1sen!2sin"
             width="100%"
             height="100%"
             loading="lazy"
